f28cd6b97f8eb6e1df992a3db4db4b1ef0a7191f
oxygenkun <oxygenkun.1@gmail.com>
2022-04-19 21:20:51 +0800

    update step 1: add option for language choice

--- src/modern_python/console.py
+++ src/modern_python/console.py
@@ -1,21 +1,28 @@
 import textwrap
+from urllib.error import HTTPError
 
 import click
 import requests
 
 from . import __version__
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+def api_utl(api_lan: str = "en") -> str:
+    return f"https://{api_lan}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
+@click.option('-l', '--language', default="en", help='Language of the app')
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-    title = data["title"]
-    extract = data["extract"]
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    try:
+        with requests.get(api_utl(language)) as response:
+            response.raise_for_status()
+            data = response.json()
+        title = data["title"]
+        extract = data["extract"]
+        click.secho(title, fg="green")
+        click.echo(textwrap.fill(extract))
+    except requests.RequestException:
+        click.secho("API is not reachable", fg="red")
