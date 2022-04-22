c58bc1f2d299b6f80da1a640de00bca56ef0bfc5
oxygenkun <oxygenkun.1@gmail.com>
2022-04-22 17:57:14 +0800

    CLI: Refactoring

--- src/modern_python/console.py
+++ src/modern_python/console.py
@@ -4,12 +4,9 @@ import locale
 import click
 import requests
 
-from . import __version__
+from . import __version__, wikipedia
 
 LAN = locale.getlocale()[0].split("_")[0]
-
-def api_utl(api_lan: str = "en") -> str:
-    return f"https://{api_lan}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
@@ -18,9 +15,7 @@ def api_utl(api_lan: str = "en") -> str:
 def main(language):
     """The hypermodern Python project."""
     try:
-        with requests.get(api_utl(language)) as response:
-            response.raise_for_status()
-            data = response.json()
+        data = wikipedia.random_page(language)
         title = data["title"]
         extract = data["extract"]
         click.secho(title, fg="green")
---
+++ src/modern_python/wikipedia.py
@@ -0,0 +1,11 @@
+import requests
+
+
+# API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+def api_url(api_lan: str = "en") -> str:
+    return f"https://{api_lan}.wikipedia.org/api/rest_v1/page/random/summary"
+
+def random_page(language: str):
+    with requests.get(api_url(language)) as response:
+        response.raise_for_status()
+        return response.json()
